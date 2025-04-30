1eeaf15996098c6abfc0cf6aeb0f9b1aa9337dbf
JoelReyes <joelhaiddreyescedillo@ciencias.unam.mx>
2025-04-30 17:26:40 -0600

    backward no completo agregado

--- Practica2/preprocesamiento/redNeuronal.py
+++ Practica2/preprocesamiento/redNeuronal.py
@@ -37,9 +37,9 @@ bigramas = lista_bigramas(corpus, palabras_a_indices)
 
 #NUEVO
 #Forward
-def forward(palabra : int, N : int, d : int, m : int):
+def forward(ind_palabra : int, N : int, d : int, m : int, ind_objetivo : int, taza_aprend : int):
     #Obtener one-hot
-    one_hot = crear_one_hot(palabra, N)
+    one_hot = crear_one_hot(ind_palabra, N)
 
     #Inicializacion
     C = np.ones((d,N))
@@ -55,7 +55,6 @@ def forward(palabra : int, N : int, d : int, m : int):
     WCi = np.dot(W, Ci)
     tanh = np.vectorize(lambda x: math.tanh(x))
     hi = tanh(WCi + b)
-
     #Pre-activacion
     ai = np.dot(U, hi) + c
 
@@ -66,12 +65,38 @@ def forward(palabra : int, N : int, d : int, m : int):
     for aj in ai:
         probas.append(math.exp(aj) / expai.sum())
     return probas
+    """
+    #backwards
+    #Capa de salida
+    dout = np.zeros((len(probas)))
+    j = ind_objetivo
+    yi = 0
+    for k in range(len(probas)):
+        if(k == j):
+            yi = 1
+        else:
+            yi = 0
+        dout[k] = (probas[k] - yi)
+    
+    #Capa oculta
+    dh = (1-(hi**2)) * (np.dot(dout, U[k,:])).sum()
+
+    du = np.dot(dh, W)
+    #Capa de emmbeding
+    dc = np.dot(du, U)
+        
+    for k in range(len(dout)):
+        U[ind_palabra, k] -= taza_aprend * np.dot(dout[k], hi)
+        W[ind_palabra, k] -= taza_aprend * np.dot(dh[k], Ci)
+        C[ind_palabra, k] -= taza_aprend * np.dot(dc[k], one_hot)
+    """
+
 
 
 #EJEMPLO
 N = len(palabras_a_indices.keys())
 for bigrama in bigramas:
     print("calculando probas para: " + str(bigrama[0]) + " = " + indices_a_palabras[bigrama[0]])
-    result = forward(bigrama[0], N, 2, 3)
+    result = forward(bigrama[0], N, 2, 3, bigrama[1], 0.5)
     print(list(palabras_a_indices.keys()))
     print(result)
